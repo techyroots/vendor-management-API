1b2e79ec7f07988f32e2f3be2d558c083b57fdb8
BlockverseInfotech <135826676+BlockverseInfotech@users.noreply.github.com>
2023-07-20 17:57:50 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,7 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true
   }],
-  serviceName: {
+  productName: {
     type: String,
     trim: true,
     required: true,
