3340300fa5821e32e64c25afb293c2359f2780c5
peeyushz <peeyush.varyani@codebird.in>
2023-07-20 20:10:00 +0530

    try

--- app.js
+++ app.js
@@ -17,9 +17,7 @@ require("dotenv").config({ path: ".variables.env" });
 
 // create our Express app
 const app = express();
-app.use(cors({
-  origin: ["https://dev-vendormanagement.web.app/", "https://dev-vendormanagement.web.app", "https://odd-culottes-lion.cyclic.app","https://odd-culottes-lion.cyclic.app/"]
-}))
+app.use(cors())
 // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -53,21 +51,21 @@ app.use((req, res, next) => {
 });
 
 // Here our API Routes
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Credentials", "true");
-  // res.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
-  // res.header("Access-Control-Expose-Headers", "Content-Length");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Accept, Authorization,x-auth-token, Content-Type, X-Requested-With, Range"
-  // );
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  } else {
-    return next();
-  }
-});
+// app.use(function (req, res, next) {
+//   // res.header("Access-Control-Allow-Origin", "*");
+//   // res.header("Access-Control-Allow-Credentials", "true");
+//   // res.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
+//   // res.header("Access-Control-Expose-Headers", "Content-Length");
+//   // res.header(
+//   //   "Access-Control-Allow-Headers",
+//   //   "Accept, Authorization,x-auth-token, Content-Type, X-Requested-With, Range"
+//   // );
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   } else {
+//     return next();
+//   }
+// });
 
 app.use("/api", authApiRouter);
 
